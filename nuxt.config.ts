9b94ed97fff9aae0d7fc2393cacf33a7ffad0bc0
Maximilian König <makoeta@gmx.de>
2026-08-25 23:03:44 +0200

    + image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  css: ["./app/assets/css/main.css"],
+  css: ["~/assets/css/main.css"],
   modules: [
     "@nuxt/content",
     "@nuxt/eslint",
